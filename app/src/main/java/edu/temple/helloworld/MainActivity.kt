1f218b740752783151b0e5d03d7ffe3e80ba12c7
Mitchell Fishman <tuq42483@temple.edu>
2024-09-23 15:41:23 -0400

    Commit 1-Fixed error #1

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -10,6 +10,8 @@ class MainActivity : AppCompatActivity() {
 
     // Declare view properties - the first one is done for you
     lateinit var displayTextView: TextView
+    lateinit var clickMeButton: Button
+    lateinit var nameEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +19,8 @@ class MainActivity : AppCompatActivity() {
 
         // Initialize with views defined in Layout - the first one is done for you
         displayTextView = findViewById(R.id.displayTextView)
+        clickMeButton = findViewById(R.id.clickMeButton)
+        nameEditText = findViewById(R.id.clickMeButton)
 
         
         findViewById<Button>(R.id.clickMeButton).setOnClickListener {
